3fafa7525bdbbe8e25e72f3d3ccfcbfb9e8455af
vladosby <vladislavn.kievski@gmail.com>
2020-01-28 23:25:55 +0300

    Add example Eq cats class

---
+++ src/main/scala/com/book/cats/EqMain.scala
@@ -0,0 +1,30 @@
+package com.book.cats
+
+import cats._
+import cats.implicits._
+
+final case class Cat(name: String, age: Int, color: String)
+
+object EqMain extends App {
+  val eqInt = Eq[Int]
+
+  println(List(1, 2, 3).map(Option(_)).filter(item => item == 1))
+  println(eqInt.eqv(123, 123))
+  println(123 === 123)
+  println(eqInt.eqv(123, 234))
+  println(123.some === none[Int])
+  //  println(List(1, 2, 3).map(Option(_)).filter(item => item === 1)) // error
+  //  println(eqInt.eqv(123, "234"))//error
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+//  implicit val catEq: Eq[Cat] = (x: Cat, y: Cat) => x == y
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (c1, c2) =>
+    c1.name === c2.name && c1.age === c2.age && c1.color === c2.color
+  }
+
+    println(s"cats ${optionCat1 === optionCat2}")
+}
