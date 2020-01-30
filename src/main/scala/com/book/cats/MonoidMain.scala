72ee33974797061c0b7f8f88e331722add9bc206
vladosby <vladislavn.kievski@gmail.com>
2020-01-30 22:52:13 +0300

    dd boolean monoind example

---
+++ src/main/scala/com/book/cats/MonoidMain.scala
@@ -0,0 +1,39 @@
+package com.book.cats
+
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+}
+trait Monoid[A] extends Semigroup[A] {
+  def empty: A
+}
+object Monoid {
+  def apply[A](implicit monoid: Monoid[A]) =
+    monoid
+}
+
+object MonoidLaws {
+  def associativeLaw[A](x: A, y: A, z: A)
+                       (implicit m: Monoid[A]): Boolean = {
+    m.combine(x, m.combine(y, z)) ==
+      m.combine(m.combine(x, y), z)
+  }
+  def identityLaw[A](x: A)
+                    (implicit m: Monoid[A]): Boolean = {
+    (m.combine(x, m.empty) == x) &&
+      (m.combine(m.empty, x) == x)
+  }
+}
+
+object MonoidInstances {
+  implicit val booleanAndMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      def combine(a: Boolean, b: Boolean) = a && b
+      def empty = true
+    }
+}
+
+
+
+object MonoidMain extends App {
+
+}
