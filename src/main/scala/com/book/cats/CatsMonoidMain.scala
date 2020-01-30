84b9a43d3be8f8d98e15f929b28f976c23dc8834
vladosby <vladislavn.kievski@gmail.com>
2020-01-30 23:18:47 +0300

    Add cats monoid example plus custom case class implementation

---
+++ src/main/scala/com/book/cats/CatsMonoidMain.scala
@@ -0,0 +1,35 @@
+package com.book.cats
+
+import cats._
+import cats.implicits._
+import MonoidInstances._
+
+case class Order(totalCost: Double, quantity: Double)
+
+object CatsMonoidMain extends App {
+  val a = Option(22)
+  val b = Option(20)
+  println(Monoid[Option[Int]].combine(a, b))
+
+  val intResult = 1 |+| 2 |+| Monoid[Int].empty
+  println(intResult)
+  println(Adder.add(List(1, 2, 3)))
+  println(Adder.add2(List(1, 2, 3)))
+  println(Adder.add2(List(1.some, none[Int], 2.some, 3.some)))
+
+  println(Order(10, 12) |+| Order(5, 5))
+}
+
+object Adder {
+  def add(items: List[Int]) = {
+    items.fold(Monoid[Int].empty)(_ |+| _)
+  }
+
+  def add2[A: Monoid](items: List[A]): A = {
+    items.fold(Monoid[A].empty)(_ |+| _)
+  }
+}
+
+object MonoidInstances {
+  implicit val orderMonoid: Monoid[Order] = Monoid.instance[Order](Order(0, 0), (o1, o2) => Order(o1.totalCost + o2.totalCost, o1.quantity + o2.quantity))
+}
