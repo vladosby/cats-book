91294dc6a8476ae4079e82c279ef049f7827451b
Uladzislau Kiyeuski <uladzislau.kiyeuski@havi.com>
2020-02-21 15:06:09 +0300

    Add imap example

--- src/main/scala/com/book/cats/CatsFunctorsMain.scala
+++ src/main/scala/com/book/cats/CatsFunctorsMain.scala
@@ -136,3 +136,51 @@ object Exercise2 extends App {
     println(format(Box("hello world")))
     println(format(Box(true)))
 }
+
+//imap
+
+object Exercise3 extends App {
+  case class Box[A](value: A)
+
+  trait Codec[A] {
+    self =>
+
+    def encode(value: A): String
+    def decode(value: String): A
+
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+      override def encode(value: B): String = self.encode(enc(value))
+
+      override def decode(value: String): B = dec(self.decode(value))
+    }
+  }
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+
+  implicit val stringCodec: Codec[String] =
+    new Codec[String] {
+      def encode(value: String): String = value
+      def decode(value: String): String = value
+    }
+
+  implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
+  implicit val booleanCodec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+
+  println(intCodec.decode("10"))
+  println(intCodec.encode(10))
+
+  implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+
+  println(doubleCodec.decode("10"))
+  println(doubleCodec.encode(10))
+
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = c.imap[Box[A]](Box(_), _.value)
+
+  println(encode(123.4))
+  // res0: String = 123.4
+  println(decode[Double]("123.4")) // res1: Double = 123.4
+  println(encode(Box(123.4)))
+  // res2: String = 123.4
+  println(decode[Box[Double]]("123.4"))
+
+}
